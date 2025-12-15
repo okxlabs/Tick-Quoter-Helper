532e07661dafafbab3eee73a9a3b9db458fbdccf
haodong.wang <haodong.wang@okg.com>
2025-12-15 14:56:51 +0800

    Deploy QueryData impl on polygon

--- scripts/deployed/polygon/index.js
+++ scripts/deployed/polygon/index.js
@@ -2,17 +2,17 @@ module.exports = {
   chainId: 137,
   proxy: "0xdea1ccaf997ec68fe2e9839a581e493d0e984a06",
   proxyAdmin: "0xbcf971dda988a5b708e5b107b5a6c9be54097967",
-  implementation: "0x643c607219513aaedc96b78b152c71e3db976ac7",
+  implementation: "0x0f573cec04483a8f344380217e9cf02a838df839",
   libraries: {
-    QueryAlgebraTicksSuperCompact: "0x7720a57fce419963108a4ca9e5131d0c39d6a556",
-    QueryZoraTicksSuperCompact: "0x7856f02e7080d507c08abb85058976761f0ab0e1",
-    QueryUniv4TicksSuperCompact: "0x1d3d302185e032e454ad50067987169d6a322c19",
-    QueryUniv3TicksSuperCompact: "0x5e916b28c62d0752a1c953d96c400efb330b6815",
-    QueryPancakeInfinityLBReserveSuperCompact: "0x600c6d5c1f15726a711d6d71a4205add668d5374",
-    QueryIzumiSuperCompact: "0x1792f5f1214ade56c08ab9689f96f2e861a5f264",
-    QueryHorizonTicksSuperCompact: "0x2a741b29212d410440a9b782ee85c8939ad7f172",
-    QueryFluidLite: "0xa86d99e80cf3cc61a256bf443483f0f75f74f57f",
-    QueryFluid: "0xac7e7a13d62d817c88531710fa308a766299f644"
+    QueryAlgebraTicksSuperCompact: "0xffcee8ff8fbba7b07a684a53ed28bc89e75ea828",
+    QueryZoraTicksSuperCompact: "0x8c734b025e5f24bfc27946d75295e02b0f5cbbef",
+    QueryUniv4TicksSuperCompact: "0x6b90c61d7c2cbda0ff969db57d1e4de8fc738f87",
+    QueryUniv3TicksSuperCompact: "0x92c3d0020d38be619ce6078a084e974bc03253a1",
+    QueryPancakeInfinityLBReserveSuperCompact: "0x20e4d526f825bc1f337d4c4dc9281496099e9322",
+    QueryIzumiSuperCompact: "0xeec24502873d77b4da1306cb5edda11bc2d12e04",
+    QueryHorizonTicksSuperCompact: "0x8495af5732603a3d54459ef45a0fc793d6d00ee3",
+    QueryFluidLite: "0xf0c959eef0d5d75bfa0fd9052a552714f48b84f8",
+    QueryFluid: "0x76e913c296cb482f07103a41bab56752d0cdaeea"
   },
   uniswapV4: {
     poolManager: "0x67366782805870060151383f4bbff9dab53e5cd6",
