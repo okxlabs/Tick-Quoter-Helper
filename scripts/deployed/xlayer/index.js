07225898aeac90052f1dbbb0907704771aa50b3f
lu.zhang <lu.zhang@okg.com>
2025-12-29 16:59:23 +0800

    deploy on xlayer

---
+++ scripts/deployed/xlayer/index.js
@@ -0,0 +1,30 @@
+module.exports = {
+  chainId: 196,
+  proxy: "0xbCF971DDA988a5B708E5b107B5A6C9Be54097967",
+  proxyAdmin: "0x643C607219513AaEdC96b78b152c71E3DB976ac7",
+  implementation: "0x073834b6cbe95822244c5d60fecb257861dea0e1",
+  libraries: {
+    QueryAlgebraTicksSuperCompact: "0x7720a57fce419963108a4ca9e5131d0c39d6a556",
+    QueryZoraTicksSuperCompact: "0x7856f02e7080d507c08abb85058976761f0ab0e1",
+    QueryUniv4TicksSuperCompact: "0x1d3d302185e032e454ad50067987169d6a322c19",
+    QueryUniv3TicksSuperCompact: "0x5e916b28c62d0752a1c953d96c400efb330b6815",
+    QueryPancakeInfinityLBReserveSuperCompact: "0x600c6d5c1f15726a711d6d71a4205add668d5374",
+    QueryIzumiSuperCompact: "0x1792f5f1214ade56c08ab9689f96f2e861a5f264",
+    QueryHorizonTicksSuperCompact: "0x2a741b29212d410440a9b782ee85c8939ad7f172",
+    QueryFluidLite: "0xa86d99e80cf3cc61a256bf443483f0f75f74f57f",
+    QueryFluid: "0xac7e7a13d62d817c88531710fa308a766299f644"
+  },
+  uniswapV4: {
+    poolManager: "0x360e68faccca8ca495c1b759fd9eee466db9fb32",
+    stateView: "0x76fd297e2d437cd7f76d50f01afe6160f86e9990",
+    positionManager: "0xbc9f3a5d767dd46e040f1ca48ab17f29f59dc806"
+  },
+  fluidLite: {
+    dex: "0x0000000000000000000000000000000000000000",
+    deployerContract: "0x0000000000000000000000000000000000000000"
+  },
+  fluid: {
+    liquidity: "0x0000000000000000000000000000000000000000",
+    dexV2: "0x0000000000000000000000000000000000000000"
+  }
+};
