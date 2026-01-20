320e432891938f0a843e1260ee7a24e93d55c56b
haodong.wang <haodong.wang@okg.com>
2026-01-20 11:15:15 +0800

    Update avax quote impl address

--- scripts/deployed/avax/index.js
+++ scripts/deployed/avax/index.js
@@ -2,17 +2,17 @@ module.exports = {
   chainId: 43114,
   proxy: "0xc0fab674ff7ddf8b891495ba9975b0fe1dcac735",
   proxyAdmin: "0xdeef773d61719a3181e35e9281600db8ba063f71",
-  implementation: "0xd22cac235c9d2a8252b0e02985a6c67a959b21b0",
+  implementation: "0xb70ff46899dded5cd1018fa446d1e78dc0dd6210",
   libraries: {
-    QueryAlgebraTicksSuperCompact: "0x7720a57fce419963108a4ca9e5131d0c39d6a556",
-    QueryZoraTicksSuperCompact: "0x7856f02e7080d507c08abb85058976761f0ab0e1",
-    QueryUniv4TicksSuperCompact: "0x1d3d302185e032e454ad50067987169d6a322c19",
-    QueryUniv3TicksSuperCompact: "0x5e916b28c62d0752a1c953d96c400efb330b6815",
-    QueryPancakeInfinityLBReserveSuperCompact: "0x600c6d5c1f15726a711d6d71a4205add668d5374",
-    QueryIzumiSuperCompact: "0x1792f5f1214ade56c08ab9689f96f2e861a5f264",
-    QueryHorizonTicksSuperCompact: "0x2a741b29212d410440a9b782ee85c8939ad7f172",
-    QueryFluidLite: "0xa86d99e80cf3cc61a256bf443483f0f75f74f57f",
-    QueryFluid: "0xac7e7a13d62d817c88531710fa308a766299f644"
+    QueryAlgebraTicksSuperCompact: "0x6daf1eae61438e3e0b1ab843188040711abe2313",
+    QueryZoraTicksSuperCompact: "0x06660e7ccd87cacd8591aed531592063fb82f255",
+    QueryUniv4TicksSuperCompact: "0xe733a820f057b7c5fa234dda8761055c1210d292",
+    QueryUniv3TicksSuperCompact: "0x78f6b54966f5997bc01c1e9e1bcc491ddec8a4de",
+    QueryPancakeInfinityLBReserveSuperCompact: "0x86894feb64ede8b5ae9a16dbd37a866de79f3149",
+    QueryIzumiSuperCompact: "0x10b8c37bd234c1f15156bcf3841207415fa0dd3d",
+    QueryHorizonTicksSuperCompact: "0xf2308615fd0e270788329f15426935009b7b75dc",
+    QueryFluidLite: "0xf0792aaebc112d5bf1282b4d8c0f667e3c386401",
+    QueryFluid: "0x6c97aba0a2b2b0de64f83b29a06bdc5f521400c3"
   },
   uniswapV4: {
     poolManager: "0x06380c0e0912312b5150364b9dc4542ba0dbbc85",
