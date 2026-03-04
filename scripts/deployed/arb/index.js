3881f70a07ff76f0016967a5e4e4f59e39065347
lu.zhang <lu.zhang@okg.com>
2026-03-04 19:04:09 +0800

    deploy stg contracts on nuiltchains

--- scripts/deployed/arb/index.js
+++ scripts/deployed/arb/index.js
@@ -1,22 +1,24 @@
 module.exports = {
   chainId: 42161,
+  version: "1.0.0",
   proxy: "0x2303669f3d9816b4cffc42ec13bf7484d284fd16",
   proxyAdmin: "0x667500c9697b475dda97ae7ba0b1a938cbc4856d",
   implementation: "0xcebb810f65141687fb2f571ff82ed728584f808d",
   libraries: {
-    QueryAlgebraTicksSuperCompact: "0x7720a57fce419963108a4ca9e5131d0c39d6a556",
-    QueryZoraTicksSuperCompact: "0x7856f02e7080d507c08abb85058976761f0ab0e1",
-    QueryUniv4TicksSuperCompact: "0x1d3d302185e032e454ad50067987169d6a322c19",
-    QueryUniv3TicksSuperCompact: "0x5e916b28c62d0752a1c953d96c400efb330b6815",
-    QueryPancakeInfinityLBReserveSuperCompact: "0x600c6d5c1f15726a711d6d71a4205add668d5374",
-    QueryIzumiSuperCompact: "0x1792f5f1214ade56c08ab9689f96f2e861a5f264",
-    QueryHorizonTicksSuperCompact: "0x2a741b29212d410440a9b782ee85c8939ad7f172",
-    QueryFluidLite: "0xa86d99e80cf3cc61a256bf443483f0f75f74f57f",
-    QueryFluid: "0xac7e7a13d62d817c88531710fa308a766299f644"
+    QueryAlgebraTicksSuperCompact: "0xd969da8f6c1f88dbc93f4ed8260b56a2017950f5",
+    QueryZoraTicksSuperCompact: "0x537a41fe75bdd9b0b8518ee0215d207327b1cc08",
+    QueryUniv4TicksSuperCompact: "0x191d584a9127fa69a0609c6c69a0994cfc54b1e6",
+    QueryUniv3TicksSuperCompact: "0xf6868efdd409b9ffa2acb8f278aec96de5591833",
+    QueryPancakeInfinityLBReserveSuperCompact: "0x14dc72517446b493ebaf5902a955ca64f32dac33",
+    QueryIzumiSuperCompact: "0x848fe4a789cb5b47471c27431c954193b0ab0fd7",
+    QueryHorizonTicksSuperCompact: "0x98d4edc84208f4ebcd3c0345c708939d35388ffe",
+    QueryFluidLite: "0xadbce1282abfc7f7ea33e8575ad51f1e98688339",
+    QueryFluid: "0xa61629840e3146646006286f1d3005167cc4e6fc"
   },
   uniswapV4: {
     poolManager: "0x360E68faCcca8cA495c1B759Fd9EEe466db9FB32",
     stateView: "0x76Fd297e2D437cd7f76d50F01AfE6160f86e9990",
     positionManager: "0xd88F38F930b7952f2DB2432Cb002E7abbF3dD869"
-  }
+  },
+  stagedImplementation: "0x316374bc97094a36170803d1aff51e1fbeed0a29"
 };
--- scripts/deployed/bsc/index.js
+++ scripts/deployed/bsc/index.js
@@ -1,22 +1,24 @@
 module.exports = {
   chainId: 56,
+  version: "1.0.0",
   proxy: "0xdea1ccaf997ec68fe2e9839a581e493d0e984a06",
   proxyAdmin: "0xbcf971dda988a5b708e5b107b5a6c9be54097967",
   implementation: "0x643C607219513AaEdC96b78b152c71E3DB976ac7",
   libraries: {
-    QueryAlgebraTicksSuperCompact: "0x8D5f9AD9ee9BE8872189b169cB0FD8f4f4880f7f",
-    QueryZoraTicksSuperCompact: "0x4D5340460e31118c33e905Fb1625a79a0a52a89a",
-    QueryUniv4TicksSuperCompact: "0x92Bd5be71B8DdD7516b36C14abCA16C715F12E3d",
-    QueryUniv3TicksSuperCompact: "0x50c6b9C6D4B738a27a4A12f055a5e29646320B2F",
-    QueryPancakeInfinityLBReserveSuperCompact: "0xC2Da508FD1d5757379677C750334D99f11c45467",
-    QueryIzumiSuperCompact: "0x6E688ABb2ECB8c299eeBEF4dc5A7389429d2e294",
-    QueryHorizonTicksSuperCompact: "0xb9C0032364D5e63CeD45Eea0f7Df189d2758B36f",
-    QueryFluidLite: "0x79C0F5f39b7e96430a68f605218A1B6c461e87f5",
-    QueryFluid: "0x8FC9B0305619b84d5272c3349Fa9eF8b4F97EAe1"
+    QueryAlgebraTicksSuperCompact: "0xd969da8f6c1f88dbc93f4ed8260b56a2017950f5",
+    QueryZoraTicksSuperCompact: "0x537a41fe75bdd9b0b8518ee0215d207327b1cc08",
+    QueryUniv4TicksSuperCompact: "0x191d584a9127fa69a0609c6c69a0994cfc54b1e6",
+    QueryUniv3TicksSuperCompact: "0xf6868efdd409b9ffa2acb8f278aec96de5591833",
+    QueryPancakeInfinityLBReserveSuperCompact: "0x14dc72517446b493ebaf5902a955ca64f32dac33",
+    QueryIzumiSuperCompact: "0x848fe4a789cb5b47471c27431c954193b0ab0fd7",
+    QueryHorizonTicksSuperCompact: "0x98d4edc84208f4ebcd3c0345c708939d35388ffe",
+    QueryFluidLite: "0xadbce1282abfc7f7ea33e8575ad51f1e98688339",
+    QueryFluid: "0xa61629840e3146646006286f1d3005167cc4e6fc"
   },
   uniswapV4: {
     poolManager: "0x28e2Ea090877bF75740558f6BFB36A5ffeE9e9dF",
     stateView: "0xd13Dd3D6E93f276FAfc9Db9E6BB47C1180aeE0c4",
     positionManager: "0x7A4a5c919aE2541AeD11041A1AEeE68f1287f95b"
-  }
+  },
+  stagedImplementation: "0x4df8e917de87c0de5bff92b73a3664412bb3ae77"
 };
