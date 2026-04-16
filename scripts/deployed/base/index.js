39eaad6d1dea2cf302cc33ece0a756fabc0ff8b5
haodong.wang <haodong.wang@okg.com>
2026-04-16 13:40:01 +0800

    Fix queryPancakeInfinityLBReserve method and update base&bsc address

--- scripts/deployed/base/index.js
+++ scripts/deployed/base/index.js
@@ -4,16 +4,16 @@ module.exports = {
   proxyAdmin: "0xeac3f383d26967952be615566360b64177f5d450",
   implementation: "0xee128587c1db1d777163119970b428886b9be2dd",
   libraries: {
+    QueryPancakeInfinityLBReserveSuperCompact: "0x6332b1Ecdabd112078d25dE72CF982C63D3890FC",
     QueryAlgebraTicksSuperCompact: "0x4DC90079EAd5dECeC19c4B05a1b8382E2977C199",
-    QueryZoraTicksSuperCompact: "0x1c03124F368035F4D8aFbC08E03d9787568C62f2",
-    QueryUniv4TicksSuperCompact: "0xa74F26c6307386b0084E857cCF3F5E31cE979941",
-    QueryUniv3TicksSuperCompact: "0x1bC7C16a1020e9D3373e3E1b12388efcd01B7703",
-    QueryPancakeInfinityLBReserveSuperCompact: "0x86894feB64Ede8B5AE9A16DbD37a866de79f3149",
-    QueryIzumiSuperCompact: "0x76Cd7f6adb3Df61A04AD34CFCC52e37ea87246dC",
-    QueryHorizonTicksSuperCompact: "0x12502F18334237928b3160397c3E5a724126c7d8",
-    QueryFluidLite: "0xf0792aaEbc112D5BF1282b4d8c0F667E3c386401",
+    QueryFluid: "0x6c97aBA0a2b2B0DE64F83b29a06bDC5f521400C3",
     QueryFluidDexV2D3D4: "0xB1c4a91e5DBBeE139Ab8CE46518C8132Ac055a7B",
-    QueryFluid: "0x6c97aBA0a2b2B0DE64F83b29a06bDC5f521400C3"
+    QueryFluidLite: "0xf0792aaEbc112D5BF1282b4d8c0F667E3c386401",
+    QueryHorizonTicksSuperCompact: "0x12502F18334237928b3160397c3E5a724126c7d8",
+    QueryIzumiSuperCompact: "0x76Cd7f6adb3Df61A04AD34CFCC52e37ea87246dC",
+    QueryUniv3TicksSuperCompact: "0x1bC7C16a1020e9D3373e3E1b12388efcd01B7703",
+    QueryUniv4TicksSuperCompact: "0xa74F26c6307386b0084E857cCF3F5E31cE979941",
+    QueryZoraTicksSuperCompact: "0x1c03124F368035F4D8aFbC08E03d9787568C62f2"
   },
   uniswapV4: {
     poolManager: "0x498581fF718922c3f8e6A244956aF099B2652b2b",
@@ -23,5 +23,5 @@ module.exports = {
   implementationHistory: [
     "0x92487d624d1f4d5dac6575dccb529728ec4c9f0d"
   ],
-  stagedImplementation: "0x1b1ea64513d8e5ba58aa3c1cf4e179db3fde4eb0"
+  stagedImplementation: "0xa6403f046f1706b48b7828701af6be807b04b739"
 };
--- scripts/deployed/bsc/index.js
+++ scripts/deployed/bsc/index.js
@@ -5,13 +5,13 @@ module.exports = {
   proxyAdmin: "0xbcf971dda988a5b708e5b107b5a6c9be54097967",
   implementation: "0x643C607219513AaEdC96b78b152c71E3DB976ac7",
   libraries: {
-    QueryFluidLite: "0xf0792aaEbc112D5BF1282b4d8c0F667E3c386401",
-    QueryFluidDexV2D3D4: "0xB1c4a91e5DBBeE139Ab8CE46518C8132Ac055a7B",
-    QueryFluid: "0x6c97aBA0a2b2B0DE64F83b29a06bDC5f521400C3",
+    QueryPancakeInfinityLBReserveSuperCompact: "0x6332b1Ecdabd112078d25dE72CF982C63D3890FC",
     QueryAlgebraTicksSuperCompact: "0x4DC90079EAd5dECeC19c4B05a1b8382E2977C199",
+    QueryFluid: "0x6c97aBA0a2b2B0DE64F83b29a06bDC5f521400C3",
+    QueryFluidDexV2D3D4: "0xB1c4a91e5DBBeE139Ab8CE46518C8132Ac055a7B",
+    QueryFluidLite: "0xf0792aaEbc112D5BF1282b4d8c0F667E3c386401",
     QueryHorizonTicksSuperCompact: "0x12502F18334237928b3160397c3E5a724126c7d8",
     QueryIzumiSuperCompact: "0x76Cd7f6adb3Df61A04AD34CFCC52e37ea87246dC",
-    QueryPancakeInfinityLBReserveSuperCompact: "0x86894feB64Ede8B5AE9A16DbD37a866de79f3149",
     QueryUniv3TicksSuperCompact: "0x1bC7C16a1020e9D3373e3E1b12388efcd01B7703",
     QueryUniv4TicksSuperCompact: "0xa74F26c6307386b0084E857cCF3F5E31cE979941",
     QueryZoraTicksSuperCompact: "0x1c03124F368035F4D8aFbC08E03d9787568C62f2"
@@ -21,5 +21,5 @@ module.exports = {
     stateView: "0xd13Dd3D6E93f276FAfc9Db9E6BB47C1180aeE0c4",
     positionManager: "0x7A4a5c919aE2541AeD11041A1AEeE68f1287f95b"
   },
-  stagedImplementation: "0x4905a35cacd6dbcc3b90c3ad1caabe19d68f7684"
+  stagedImplementation: "0x263d3bfe06de8eb835a5c3af391f6f4e3a8251ec"
 };
